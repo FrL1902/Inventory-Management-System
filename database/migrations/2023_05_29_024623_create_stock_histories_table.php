47d4fb2aefc980adb2901ee8c5f860824605dc42
FrL1902 <farrel.rasyad@binus.ac.id>
2023-05-29 10:22:21 +0700

    added all things history, backend mostly

---
+++ database/migrations/2023_05_29_024623_create_stock_histories_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_histories', function (Blueprint $table) {
+            $table->id();
+            $table->string('item_name');
+            $table->integer('stock_before');
+            $table->integer('stock_added');
+            $table->integer('stock_taken');
+            $table->integer('stock_now');
+            $table->string('user_who_did');
+            $table->timestamps();
+        });
+    }
+
+    // History ID : IDnya kyknya gini aja deh, TRSN1,  "TRSN" nya kode awal, angka belakangnya increment berdasarkan ID //buat skrg pake id biasa aja dulu
+    //                                             Item name // jelas kali ya darimana, figure it out
+    //                                             Stock Before // jelas kali ya darimana, figure it out
+    //                                             Stock Added // jelas kali ya darimana, figure it out
+    //                                             Stock Taken // jelas kali ya darimana, figure it out
+    //                                             Stock Now // jelas kali ya darimana, figure it out
+    //                                             Updated At : ini tunjukin created at
+    //                                             By User : ini pake auth
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_histories');
+    }
+}
