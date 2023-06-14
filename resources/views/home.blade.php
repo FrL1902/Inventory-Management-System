dd7d08cd1302986b95a8d5c7d0eba8a8365ab07a
FrL1902 <farrel.rasyad@binus.ac.id>
2023-06-14 15:33:39 +0700

    deleted some cringe sht

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,37 +6,13 @@
 
 <div class="main-panel">
     <div class="content">
-        ini home / nothin's here yet for now
+        ini home page
+
+        {{-- / nothin's here yet for now
         test home
         second commit
 
         DIBAWAH INI MODALNYA BISA, JADI KYK SWEETALERT POP UPS GITU
-        {{--
-        <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div> --}}
 
         <div class="alert alert-success alert-block">
             <button type="button" class="close" data-dismiss="alert">×</button>
@@ -45,9 +21,35 @@
 
         <div class="alert alert-success" role="alert">
             This is a success alert—check it out!
-        </div>
+        </div> --}}
     </div>
 
+    {{--
+            <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+      Launch demo modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div> --}}
 
     @if ($message = Session::get('sukses'))
         <div class="alert alert-success alert-block">
