75099dc664a84c37d605ba823d6d1646725d6b4a
FrL1902 <farrel.rasyad@binus.ac.id>
2023-06-15 15:11:56 +0700

    changed time zone

--- config/app.php
+++ config/app.php
@@ -67,7 +67,10 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    // 'timezone' => 'UTC',
+    'timezone' => 'Asia/Jakarta',
+
+    // https://www.php.net/manual/en/timezones.asia.php
 
     /*
     |--------------------------------------------------------------------------
@@ -166,6 +169,7 @@ return [
          * Package Service Providers...
          */
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -230,7 +234,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
-
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
