4fc4867cd32e9504b0576f027f89a4db744c3d5c
FrL1902 <farrel.rasyad@binus.ac.id>
2023-06-21 10:50:24 +0700

    formatted excel output date, border, text aligns

--- app/Exports/IncomingExport.php
+++ app/Exports/IncomingExport.php
@@ -13,6 +13,7 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Events\AfterSheet;
 use PhpOffice\PhpSpreadsheet\Shared\Drawing;
+use PhpOffice\PhpSpreadsheet\Style\ConditionalFormatting\Wizard;
 
 class IncomingExport implements FromCollection, WithHeadings, ShouldAutoSize, WithMapping, WithEvents
 
@@ -36,7 +37,7 @@ class IncomingExport implements FromCollection, WithHeadings, ShouldAutoSize, Wi
     public function headings(): array
     {
         return [
-            'ID', 'customer name', 'brand name', 'item name', 'stock before', 'stock added', 'stock now', 'description', 'picture link', 'time (WIB)'
+            'ID', 'Customer Name', 'Brand Name', 'Item Name', 'Stock Before', 'Stock Added', 'Stock Now', 'description', 'picture link', 'time (WIB)'
         ];
     }
 
@@ -51,7 +52,15 @@ class IncomingExport implements FromCollection, WithHeadings, ShouldAutoSize, Wi
 
     public function map($item): array
     {
+
+        // $formattedDate = date_format($item->created_at, "D/d/m/y H:i:s");
+
+        // dd($formattedDate);
+        // dd($item);
+
         return [
+            // $formattedDate = date_format($item->created_at, "D/d/m/y H:i:s"),
+
             $item->id,
             $item->customer->customer_name,
             $item->brand->brand_name,
@@ -61,17 +70,61 @@ class IncomingExport implements FromCollection, WithHeadings, ShouldAutoSize, Wi
             $item->stock_now,
             $item->description,
             $item->picture_link,
-            $item->created_at
+            // $$item->created_at
+            date_format($item->created_at, "D/d/m/y H:i:s"),
+
         ];
     }
 
     public function registerEvents(): array
     {
+
+        // dd($lastData);
+
         return [
             AfterSheet::class    => function (AfterSheet $event) {
                 $event->sheet->getStyle('A1:J1')->applyFromArray([
                     'font' => ['bold' => true]
                 ]);
+                $styleArrayHeading = [
+                    'borders' => [
+                        'allBorders' => [ //ni piliannya bukan 'allBorders' doang, ada left, right, outline, inside, dll, cek disini https://phpspreadsheet.readthedocs.io/en/latest/topics/recipes/#styling-cell-borders
+                            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                            'color' => ['rgb' => '000000'], //ini lgsg pake hex aja, terus value di arraynya diganti ke rgb, bisa cari online pake color picker
+                            // 'color' => ['argb' => 'ff000000'], //ini argb, jadi value rgb di convert ke argb, bisa pake ini https://www.myfixguide.com/color-converter/
+                        ],
+                    ],
+                ];
+                $styleArrayContent = [
+                    'borders' => [
+                        'allBorders' => [
+                            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                            'color' => ['rgb' => '000000'],
+                        ],
+                    ],
+                ];
+
+                $frontTolastData = 'A2:J' . strval(count($this->incomingData) + 1);
+                $event->sheet->getStyle('A1:J1')->applyFromArray($styleArrayHeading);
+                $event->sheet->getStyle($frontTolastData)->applyFromArray($styleArrayContent);
+                // $event->sheet->getStyle('A1:J1')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getStyle('A1:J1')->getAlignment()->setHorizontal('center'); //set heading horizontal alignment to center
+                $event->sheet->getStyle($frontTolastData)->getAlignment()->setHorizontal('left'); //set data below heading alignment to left
+
+
+                // tes hyperlink
+                // $frontTolastDataHyperlink = 'K2:K' . strval(count($this->incomingData) + 1);
+                // $cellRange = $frontTolastDataHyperlink;
+                // $conditionalStyles = [];
+                // $wizardFactory = new Wizard($cellRange);
+                // /** @var Wizard\CellValue $cellWizard */
+                // $cellWizard = $wizardFactory->newRule(Wizard::CELL_VALUE);
+                // $cellWizard->equals('HYPERLINK(I1)', Wizard::VALUE_TYPE_FORMULA);
+                // $conditionalStyles[] = $cellWizard->getConditional();
+
+                // $event->sheet->getStyle($cellWizard->getCellRange())->setConditionalStyles($conditionalStyles);
+
+                // $event->sheet->setCellValue($frontTolastDataHyperlink, '=HYPERLINK(I2)');
             },
 
         ];
