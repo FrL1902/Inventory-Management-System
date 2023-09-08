f3ad8010621c678ff0e2b81afe07d6e246372eb6
FrL1902 <farrel.rasyad@binus.ac.id>
2023-09-08 09:20:57 +0700

    history pallet size responsive fix

--- resources/views/palletHistory.blade.php
+++ resources/views/palletHistory.blade.php
@@ -10,7 +10,6 @@
     <div class="main-panel">
         <div class="content">
             <div class="page-inner">
-                ini page buat liat sejarah palet
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card">
@@ -22,7 +21,7 @@
                                                 <th style="width: 30%">Nama Barang</th>
                                                 <th>Stok</th>
                                                 <th>BIN</th>
-                                                <th>Status</th>
+                                                <th style="display: block">Status</th>
                                                 <th>Waktu</th>
                                                 <th>User</th>
                                             </tr>
@@ -45,21 +44,21 @@
                                                     <td>{{ $pallet->stock }}</td>
                                                     <td>{{ $pallet->bin }}</td>
                                                     @if ($pallet->status == 'KELUAR')
-                                                        <td style="text-align: center"><strong>
+                                                        <td style="display: block; min-width:200px; text-align: center;"><strong>
                                                                 <p
                                                                     style="margin: auto; color: white; background-color: rgb(189, 66, 55);border-radius: 25px">
                                                                     {{ $pallet->status }}</p>
                                                             </strong>
                                                         </td>
                                                     @elseif ($pallet->status == 'DALAM INVENTORY')
-                                                        <td style="text-align: center"><strong>
+                                                        <td style="display: block; text-align: center; min-width:200px;"><strong>
                                                                 <p
                                                                     style="margin: auto; color: white; background-color: rgb(55, 111, 189);border-radius: 25px">
                                                                     {{ $pallet->status }}</p>
                                                             </strong>
                                                         </td>
                                                     @elseif (str_contains($pallet->status, 'SEBAGIAN'))
-                                                        <td style="text-align: center"><strong>
+                                                        <td style="display: block; text-align: center; min-width:200px;"><strong>
                                                                 <p
                                                                     style="margin: auto; color: white; background-color:rgb(189, 173, 55);border-radius: 25px">
                                                                     {{ $pallet->status }}</p>
