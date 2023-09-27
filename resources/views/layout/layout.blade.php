b24e9cd3e4e04decdf7db3ce14427702f2735aa5
FrL1902 <farrel.rasyad@binus.ac.id>
2023-09-27 15:16:42 +0700

    none

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -105,10 +105,11 @@
 
     #overlayPage {
         position: fixed;
+        display:none;
         top: 0;
         left: 0;
-        width: 100vw;
-        height: 100vh;
+        width: 100%;
+        height: 100%;
         background: black;
         opacity: 0.2;
         z-index: 1;
